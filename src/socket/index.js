9f8eaad5c2691343a21d03fa4e3bb40f8bd6ec08
rahimov19 <rahimov19.ar@gmail.com>
2023-02-15 14:21:33 +0100

    added chat

---
+++ src/socket/index.js
@@ -0,0 +1,26 @@
+let onlineUsers = [];
+
+export const newConnectionhandler = (newClient) => {
+  console.log("NEW CONNECTION:", newClient.id);
+
+  newClient.emit("welcome", { message: `Hello ${newClient.id}` });
+
+  newClient.on("setUsername", (payload) => {
+    console.log(payload);
+    onlineUsers.push({ username: payload.username, socketId: newClient.id });
+
+    newClient.emit("loggedIn", onlineUsers);
+
+    newClient.broadcast.emit("updatedOnlineUsersList", onlineUsers);
+  });
+
+  newClient.on("sendMessage", (message) => {
+    console.log("NEW MESSAGE:", message);
+    newClient.broadcast.emit("newMessage", message);
+  });
+
+  newClient.on("disconnect", () => {
+    onlineUsers = onlineUsers.filter((user) => user.socketId !== newClient.id);
+    newClient.broadcast.emit("updateOnlineUsersList", onlineUsers);
+  });
+};
